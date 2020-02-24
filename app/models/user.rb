cc730e56172004ed5470b423b3cb867cb2174b90
ProgHazem <hazemabuelala10@gmail.com>
2020-02-24 22:09:33 +0200

    init api blog

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  extend Devise::Models
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+  # mount image
+  mount_uploader :image, ImageUploader
+end
